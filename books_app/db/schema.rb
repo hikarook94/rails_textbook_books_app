7a365cf0e015ff126ca7adde2487f4ed8c76976b
uhikarudevjapan <walrus4l145@icloud.com>
2022-04-19 16:14:51 +0900

    画像アップロード機能を追加

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_19_070102) do
+ActiveRecord::Schema.define(version: 2022_04_19_070708) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_04_19_070102) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
